0fec058c2b966cc61932f4b9cd4dfe537839ef99
Joao Melo <jopcmelo@gmail.com>
2021-05-14 11:36:12 -0300

    feat: validating permissions from server

--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -4,21 +4,51 @@ import {
   GetServerSidePropsResult,
 } from "next";
 import { destroyCookie, parseCookies } from "nookies";
+import decode from "jwt-decode";
 import { AuthError } from "../errors/AuthError";
+import { validateUserPermissions } from "./validateUserPermissions";
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function withSSRAuth<P>(
+  fn: GetServerSideProps<P>,
+  options: WithSSRAuthOptions
+): GetServerSideProps {
   return async (
     context: GetServerSidePropsContext
   ): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(context);
+    const token = cookies["nextauth.token"];
 
-    if (!cookies["nextauth.token"]) {
+    if (!token) {
       return {
         redirect: {
           destination: "/",
           permanent: false,
         },
       };
+    }
+
+    if (options) {
+      const user = decode<{ permissions: string[]; roles: string[] }>(token);
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions: options.permissions,
+        roles: options.roles,
+      });
+
+      if (!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: "/dashboard",
+            permanent: false,
+          },
+        };
+      }
     }
 
     try {
